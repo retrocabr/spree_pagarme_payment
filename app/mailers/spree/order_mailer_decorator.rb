c3d56d6255fbc03e9904866d6f944ab4084bab97
Denise Fugihara <denise@retroca.com.br>
2016-08-02 20:58:30 -0300

    fix bug

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 Spree::OrderMailer.class_eval do
 
-    def error_notification(subject, message)
+    def pagarme_error_notification(subject, message)
       mail(to: ENV['ADMIN_EMAILS'].split(","), subject: subject, body: message)
     end
 
