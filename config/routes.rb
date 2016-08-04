7808a39a9b72bb8c7d31919557968dfdf11d387f
Denise Fugihara <denise@retroca.com.br>
2016-08-04 15:48:00 -0300

    fix postback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Spree::Core::Engine.routes.draw do
-	post "orders/:order_id/postback", :to => "pagarme_payment#postback" #, :as => :pagarme_postback
+	post "orders/:order_id/postback", :to => "orders#postback" #, :as => :pagarme_postback
 end
